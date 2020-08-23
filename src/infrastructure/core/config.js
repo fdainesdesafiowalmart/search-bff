62042ac6506c1c03fb8f1ca74dade59226772893
Francisco Daines <francisco.daines@2brains.cl>
2020-08-22 22:06:54 -0400

    Fixes PORT environment variable

--- src/infrastructure/core/config.js
+++ src/infrastructure/core/config.js
@@ -3,7 +3,7 @@ const configuration = {
     id: 'search-bff',
     name: 'Search BFF',
     prefixURL: '/search-bff/v1',
-    port: process.env.NODE_PORT || 3000,
+    port: process.env.PORT || 3000,
     logLevel: process.env.LOG_LEVEL || 'debug'
   }
 }
